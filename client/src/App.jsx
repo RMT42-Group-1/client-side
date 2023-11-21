dbd5d37ed620235fe37f34b32fdd16b3d9679875
fauzandwip <fauzan27dp@gmail.com>
2023-11-21 18:50:21 +0700

    slicing navbar add handle on logout create MainLayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './router';
 
 function App() {
-	return <div className="main bg-slate-600 text-2xl text-white">Main</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
