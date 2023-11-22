d08d780eac14c963a3aec425bb4b1e30980136d0
fauzandwip <fauzan27dp@gmail.com>
2023-11-22 20:52:33 +0700

    feat: add redux, react-redux, and @reduxjs/toolkit packages create store for redux create state management for leaderboard page using redux toolkit setup react-redux

---
+++ client/src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import leaderboardReducer from './features/leaderboard/leaderboardSlice';
+
+export const store = configureStore({
+	reducer: {
+		leaderboard: leaderboardReducer,
+	},
+});
